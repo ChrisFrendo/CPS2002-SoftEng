4fa6dd3f7bc567621757ad5c02a248775c446c88
manwelbugeja <manwel.bugeja.18@um.edu.mt>
2020-04-06 11:10:44 +0200

    Added junit test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,2 +1,5 @@
 public class HelloWorld {
+    public String helloWorld() {
+        return "Hello, World!";
+    }
 }
--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,5 +1,24 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HelloWorldTest {
+    HelloWorld helloWorld;
+    @Before
+    public void setUp() {
+        helloWorld = new HelloWorld();
+    }
 
+    @After
+    public void tearDown() {
+        helloWorld = null;
+    }
+
+    @Test
+    public void helloWorldTest() {
+        String expected = "Hello, World!";
+        String response = helloWorld.helloWorld();
+        assertEquals(expected, response);
+    }
 }
