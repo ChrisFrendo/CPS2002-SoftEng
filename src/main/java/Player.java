49307162378c9eed4d215f0cd6a7aac88118ef3e
manwelbugeja <manwel.bugeja.18@um.edu.mt>
2020-04-11 18:57:42 +0200

    removed redundant classes

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,5 +1,3 @@
-import javafx.geometry.Pos;
-
 public class Player {
 
     private static class Position {
@@ -16,5 +14,15 @@ public class Player {
     public int getPositionX() { return Position.x; }
 
     public int getPositionY() { return Position.y; }
+
+    public boolean setPosition(int newX, int newY) {
+        if (newX >= 0 && newY >= 0) {
+            Position.x = newX;
+            Position.y = newY;
+            return true;
+        }
+
+        return false;
+    }
 }
 
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,8 +1,8 @@
-import static org.junit.Assert.assertEquals;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class PlayerTest {
     private Player player1;
@@ -19,6 +19,39 @@ public class PlayerTest {
 
     @Test
     public void initialPosition() {
+        int expectedX = 0;
+        int responseX = player1.getPositionX();
+
+        assertEquals(expectedX, responseX);
+
+        int expectedY = 0;
+        int responseY = player1.getPositionY();
+
+        assertEquals(expectedY, responseY);
+    }
+
+    @Test
+    public void setPositionSuccess() {
+        boolean response = player1.setPosition(1, 2);
+
+        assertTrue(response);
+
+        int expectedX = 1;
+        int responseX = player1.getPositionX();
+
+        assertEquals(expectedX, responseX);
+
+        int expectedY = 2;
+        int responseY = player1.getPositionY();
+
+        assertEquals(expectedY, responseY);
+    }
+
+    @Test
+    public void setPositionFailure() {
+        boolean response = player1.setPosition(-1, 0);
+        assertFalse(response);
+
         int expectedX = 0;
         int responseX = player1.getPositionX();
 
