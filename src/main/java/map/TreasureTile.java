04afd0e255eb29001ca17e6222a90a9aea52d9ed
ChrisFrendo <chris.frendo.18@um.edu.mt>
2020-04-12 17:36:37 +0200

    Added getHtml method to Tile to return html element for each type of Tile

--- src/main/java/map/TreasureTile.java
+++ src/main/java/map/TreasureTile.java
@@ -2,4 +2,10 @@ package map;
 
 public class TreasureTile extends Tile {
 
+    private static final String HTML = "<img alt=\"Grass tile\" src=\"images/tiles/TreasureTile.png\"";
+
+    @Override
+    public String getHtml() {
+        return HTML;
+    }
 }
