173ef4d6ce0e81da93b8d74f462678f5b2d14ccc
ChrisFrendo <chris.frendo.18@um.edu.mt>
2020-04-18 18:05:38 +0200

    Fixed bug regarding mapsize input

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -12,20 +12,21 @@ public class Menu {
         /* Prompts for amount of players */
         int playerAmount = 0;
         System.out.println("Enter number of players:");
-        while(!gameEngine.validatePlayers(playerAmount)) {
+        while (!gameEngine.validatePlayers(playerAmount)) {
             System.out.println("Please enter an amount from 2 to 8");
             playerAmount = scanner.nextInt();
         }
 
         /* Prompts for map size */
-        int mapSize = 0;
-        System.out.println("Enter map size");
-        while(gameEngine.createMap(mapSize, playerAmount) != null) {
+        int mapSize;
+        do {
+            System.out.println("Enter map size");
             System.out.println("Please enter a maximum of 50");
             System.out.println("and a minimum of 5 for 2-4 player");
             System.out.println("a minimum of 8 for 5-8 players");
             mapSize = scanner.nextInt();
-        }
+        } while (gameEngine.createMap(mapSize, playerAmount) == null);
+
 
         /* Creates Players */
         gameEngine.createPlayers(playerAmount);
