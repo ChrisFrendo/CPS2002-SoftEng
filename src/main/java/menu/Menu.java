891dfaf8c0ab602031dec548057f62f0f1ac7da0
ChrisFrendo <chris.frendo.18@um.edu.mt>
2020-04-22 13:35:32 +0200

    Updaated menu

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -2,6 +2,7 @@ package menu;
 
 import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import map.Tile;
@@ -10,7 +11,7 @@ import utils.Color;
 import utils.FileHelperUtils;
 
 public class Menu {
-    private static int playerAmount = 0;
+    static int playerAmount = 0;
     private static Tile[][] gameBoard = null;
     private static Scanner scanner = new Scanner(System.in);
     private static GameEngine gameEngine = GameEngine.getInstance();
@@ -19,8 +20,17 @@ public class Menu {
         System.out.print(Color.CYAN_BOLD_BRIGHT);
         System.out.println("Welcome to Water tiles!!");
         System.out.print(Color.RESET);
+
         /* calling helper function to get number of players and map size*/
-        setupGameValues();
+        while (true) {
+            try {
+                setupGameValues();
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                continue;
+            }
+            break;
+        }
 
         System.out.println("Starting Game");
 
@@ -30,89 +40,101 @@ public class Menu {
 
         FileHelperUtils.deleteDirectory("generatedHTML");
 
-        /* Generate initial positions in html */
-        for (int i = 0; i < playerList.size(); ++i) {
-            gameEngine.writeHtml(gameBoard, playerList.get(i), i);
-        }
+        generateHtml(playerList);
 
         /* Moves players */
-        char input;
-        int currentPlayerNumber;
         boolean treasureFound = false;
 
 
         while (!treasureFound) {
             System.out.println("================================================");
             System.out.println("Generated Updated Maps");
-            /* Getting moves for all players */
-            for (currentPlayerNumber = 0; currentPlayerNumber < playerList.size(); ++currentPlayerNumber) {
-                System.out.print(Color.MAGENTA_UNDERLINED);
-                System.out.println(playerList.get(currentPlayerNumber).getId());
-                System.out.print(Color.RESET);
-                System.out.println("Enter move [u, d, l, r]:");
-                input = scanner.next().charAt(0);
 
-                if (!gameEngine.handleInput(input, currentPlayerNumber)) {
-                    System.out.print(Color.RED);
-                    System.out.println("Cannot move in that direction. Enter a different move");
-                    System.out.print(Color.RESET);
-                    --currentPlayerNumber;
-                }
+            getPlayerMoves(playerList);
 
-            }
+            generateHtml(playerList);
 
-            /* Generating html for each player */
-            for (currentPlayerNumber = 0; currentPlayerNumber < playerList.size(); currentPlayerNumber++) {
-                gameEngine.writeHtml(gameBoard, playerList.get(currentPlayerNumber), currentPlayerNumber);
-            }
-
-            /* Checking if there are any winners */
-            for (currentPlayerNumber = 0; currentPlayerNumber < playerList.size(); currentPlayerNumber++) {
-                /* Check if treasure is found */
-                if (playerList.get(currentPlayerNumber).isWinner()) {
-                    treasureFound = true;
-                    System.out.print(Color.YELLOW_BOLD);
-                    System.out.println("Player " + currentPlayerNumber + " found the treasure");
-                    System.out.print(Color.RESET);
-                }
-            }
+            treasureFound = checkWinners(playerList);
         }
     }
 
-    private static void setupGameValues() {
+    static void getPlayerMoves(List<Player> playerList) {
+        char input;
+
+        /* Getting moves for all players */
+        for (int i = 0; i < playerList.size(); ++i) {
+            System.out.print(Color.MAGENTA_UNDERLINED);
+            System.out.println(playerList.get(i).getId());
+            System.out.print(Color.RESET);
+            System.out.println("Enter move [u, d, l, r]:");
+            input = scanner.next().charAt(0);
+
+            if (!gameEngine.handleInput(input, i)) {
+                System.out.print(Color.RED);
+                System.out.println("Cannot move in that direction. Enter a different move");
+                System.out.print(Color.RESET);
+                --i;
+            }
+
+        }
+    }
+
+    static void setupGameValues() {
         System.out.println("================================================");
         /* Prompts for amount of players */
         System.out.println("Enter number of players:");
         while (!gameEngine.validatePlayers(playerAmount)) {
             System.out.println("Please enter an amount from 2 to 8");
-            try {
-                playerAmount = scanner.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input");
-            } finally {
-                scanner.nextLine();
-            }
+            playerAmount = getIntInput();
+            scanner.nextLine();
         }
 
         /* Prompts for map size */
-        int mapSize;
-
         System.out.println("Enter map size");
         while (gameBoard == null) {
             System.out.println("Please enter a maximum of 50");
             System.out.println("and a minimum of 5 for 2-4 player");
             System.out.println("a minimum of 8 for 5-8 players");
-            try {
-                mapSize = scanner.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input");
-                gameBoard = null;
-                scanner.nextLine();
-                continue;
-            }
+            int mapSize = getIntInput();
+            scanner.nextLine();
             gameBoard = gameEngine.createMap(mapSize, playerAmount);
         }
-        scanner.nextLine();
+
         System.out.println("================================================");
+    }
+
+    private static int getIntInput() {
+        int value;
+        try {
+            value = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.print(Color.RED);
+            System.out.println("Invalid Input");
+            System.out.print(Color.RESET);
+            throw e;
+        }
+        return value;
+    }
+
+    static void generateHtml(List<Player> playerList) {
+        /* Generating html for each player */
+        for (int i = 0; i < playerList.size(); i++) {
+            gameEngine.writeHtml(gameBoard, playerList.get(i), i);
+        }
+    }
+
+    static boolean checkWinners(List<Player> playerList) {
+        boolean treasureFound = false;
+        /* Checking if there are any winners */
+        for (int i = 0; i < playerList.size(); i++) {
+            /* Check if treasure is found */
+            if (playerList.get(i).isWinner()) {
+                treasureFound = true;
+                System.out.print(Color.YELLOW_BOLD);
+                System.out.println("Player " + i + " found the treasure");
+                System.out.print(Color.RESET);
+            }
+        }
+        return treasureFound;
     }
 }
