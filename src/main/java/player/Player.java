86611f7546736fdb8276e311ba1fc31ec76c1a54
ChrisFrendo <chris.frendo.18@um.edu.mt>
2020-05-28 18:22:10 +0200

    Made Team and Player Subject and Oberver respectively

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -6,27 +6,26 @@ import java.util.Random;
 
 import map.Map;
 import map.Tile;
+import team.Observer;
+import team.Team;
 
 /**
  * Class that represents a player in the game
  */
-public class Player {
+public class Player extends Observer<Team> {
 
     private Position currentPosition;
     Position startingPosition;
     private String id;
-    private boolean winner;
-    private List<Tile> visitedTiles;
-    private List<Direction> pastMoves;
+    private boolean winner = false;
+    private List<Tile> visitedTiles = new ArrayList<>();
+    private List<Direction> pastMoves = new ArrayList<>();
 
     public Player(String id) {
         this.id = id;
-        this.winner = false;
         this.startingPosition = generateRandomPosition(new Random());
         this.currentPosition = new Position(this.startingPosition);
-        this.visitedTiles = new ArrayList<>();
         this.visitedTiles.add(Map.getInstance().getTile(this.startingPosition.getRow(), this.startingPosition.getColumn()));
-        this.pastMoves = new ArrayList<>();
     }
 
     /**
@@ -47,6 +46,11 @@ public class Player {
         return visitedTiles;
     }
 
+    /**
+     * Getter
+     *
+     * @return list of directions which the player has taken
+     */
     public List<Direction> getPastMoves() {
         return pastMoves;
     }
@@ -102,6 +106,7 @@ public class Player {
         int newRow;
         int newColumn;
 
+        // computing the values of newRow and newColumn based on the director
         switch (direction) {
             case up:
                 newRow = this.currentPosition.getRow() - 1;
@@ -120,21 +125,82 @@ public class Player {
                 newColumn = this.currentPosition.getColumn() + 1;
         }
 
+        // if tile exists, process move
         if (Map.getInstance().tileExists(newRow, newColumn)) {
             Tile.Status tileStatus = Map.getInstance().getTileStatus(newRow, newColumn);
+
+            // check status of tile
             if (tileStatus.equals(Tile.Status.GRASS)) {
+                // player can go on grass
                 this.currentPosition.setPosition(newRow, newColumn);
             } else if (tileStatus.equals(Tile.Status.WATER)) {
+
+                // if there is a subject, set its state to the water tile's position
+                if (this.subject != null)
+                    this.subject.setState(new Position(newRow, newColumn));
+
+                // reset to starting position
                 this.currentPosition = new Position(this.startingPosition);
             } else {
+                // updating current position and setting player as winner
                 this.currentPosition.setPosition(newRow, newColumn);
                 this.winner = true;
             }
+
+            // adding tile to visitedTiles
             this.visitedTiles.add(Map.getInstance().getTile(newRow, newColumn));
+
+            // adding direction to pastMoves
             this.pastMoves.add(direction);
+
+            // if there is a subject, set its state the the current position to update map visibility
+            if (this.subject != null)
+                this.subject.setState(this.currentPosition);
+
             return true;
         } else {
             return false;
         }
+    }
+
+    /**
+     * Overridden update method from Observer class
+     * <p>
+     * Adds the position from the subject's state to the list of visitedTiles if it is not already there
+     * This allows players attached to a subject (part of the same team) to view each other's visible tiles
+     */
+    @Override
+    public void update() {
+        Position position = this.subject.getState();
+        Tile newTile = Map.getInstance().getTile(position.getRow(), position.getColumn());
+        if (!this.visitedTiles.contains(newTile))
+            this.visitedTiles.add(newTile);
+    }
+
+    /**
+     * Attaches a given team as the instance's subject
+     *
+     * @param team The Team which the player will belong to
+     */
+    public void setTeam(Team team) {
+        this.subject = team;
+        this.subject.attach(this);
+    }
+
+    /**
+     * Getter
+     *
+     * @return returns the subject of the player, which is the player's team
+     */
+    public Team getTeam() {
+        return this.subject;
+    }
+
+    /**
+     * Sets the state of the Team subject with the player's startingPosition
+     * This ensures all team members can see each other's starting positions
+     */
+    public void attachStartPosition() {
+        this.subject.setState(this.startingPosition);
     }
 }
