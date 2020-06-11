6a79643635d121bb88d690969fc410c2f6488d53
manwelbugeja <manwel.bugeja.18@um.edu.mt>
2020-06-11 19:49:10 +0200

    added unit tests for SafeMap and SafeMapCreator classes

---
+++ src/test/java/map/SafeMapCreatorTest.java
@@ -0,0 +1,14 @@
+package map;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class SafeMapCreatorTest {
+    @Before
+    void setUp() {
+    }
+
+    @After
+    void tearDorn() {
+    }
+}
---
+++ src/test/java/map/SafeMapTest.java
@@ -0,0 +1,18 @@
+package map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SafeMapTest {
+    private Map map = MapCreator.getMapInstance("safe");
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+}
